6eceb5693c23df4ec2aedf116463c292993bbea2
lincoln <haggard.lincoln@gmail.com>
2024-12-17 19:13:50 -0700

    significantly improved the readability of the main function

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import pygame
 import sys
 from menu import Menu
 from game import Game
+
 pygame.init()
 clock=pygame.time.Clock()   
 screen=pygame.display.set_mode((2560, 1400), flags=pygame.RESIZABLE ,vsync=1)
-menu=Menu(screen,clock)
-game=Game(screen,clock,menu)
+
+menu=Menu(screen,clock) #the menu init function also starts it's mainloop
+game=Game(screen,clock,menu) #the game init function also starts it's' mainloop
+
 while True:
-    menu.resizeall(screen)
+    menu.resizeall(screen) #don't do the menu init function otherwise the user's previous choices will be reset
     menu.mainloop(screen,clock,game)
     game.__init__(screen,clock,menu)
-    game.mainloop(screen,clock) 
